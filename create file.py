e3d5d57e9992e2136daa442c7cd2a848f1264f4b
nampy3 <16tec2cs017@vgu.ac.in>
2019-06-09 19:11:18 +0530

    file handling

---
+++ create file.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+
+#creating a file
+
+f=open('hello.txt','w')
+f.write("hello world this is file writer")
+f.write("\n")
+f.write("hello word")
+f.close()
+
---
+++ read file.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/python3
+
+# read operation
+
+f=open('hello.txt','r')
+data=f.read()
+print(data)
+f.close()
+
