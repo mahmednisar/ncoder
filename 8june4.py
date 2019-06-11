c0fa29bbbf47d227fac9068801eb49ee6b5854dc
Nisar Ahmed <16tec2cs017@vgu.ac.in>
2019-06-11 22:17:01 +0530

    Update 8june4.py

--- 8june4.py
+++ 8june4.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import os
 import crypt
-username=input("Enter your Username : ")
+usrname=input("Enter your Username : ")
 flag=0
 num=[0,1,2,3,4,5,6,7,8,9]
-for i in username:
+for i in usrname:
 	if i in str(num):
 		flag=1
 if(flag==1):
 	print("Invalid User Name")
 else:
-	password="hello"+username
-	encPass = crypt.crypt(password,"22") #Encrypting Password
-	os.system("useradd -p " + encPass +" "+username)
+	pwd="hello"+usrname
+	encpwd = crypt.crypt(pwd,"22") #Encrypting Password
+	os.system("useradd -p " + encpwd +" "+usrname)
 	print("User Added!")
 
