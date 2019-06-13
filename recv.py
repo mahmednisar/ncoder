6bc7857b42b48f28d506313a25df9e56f379336b
nampy3 <16tec2cs017@vgu.ac.in>
2019-06-13 20:10:28 +0530

    one-one receiver

--- recv.py
+++ recv.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python2
 
-import socket
+import  socket 
 recv_ip="127.0.0.1"
-recv_port=4444  #0-1024 --you check free udp port netstat -nulp
+recv_port=4444  #    0 - 1024  -- you can check free udp port netstat -nulp
 
-#creating udp socket 
-#           ip type     v4        udp
+#  creating  udp socket
+#               ip type v4 ,  uDp  
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-#fitting  ip and port with udp socket 
+#  fitting    ip and port  with udp socket 
 s.bind((recv_ip,recv_port))
 
-#recv data from sender 
-while 4>2:
-	data=s.recvfrom(256)
-	print("Message from sender" , data[0])
-	print("Sender IP + port --socket" , data[0])
-	reply=raw_input("type your reply")
-	sendto(reply,data[1])
+#   recv  data  from  sender  
+while  4  >  2  :
+    data=s.recvfrom(100)  
+    print("message  from sender  ",data[0])
+    print("sender IP + port --socket  ",data[1])
+    #  reply to sender  
+    rply=raw_input("type your rply  : ")
+    s.sendto(rply,data[1])
+
 
 s.close()
+
