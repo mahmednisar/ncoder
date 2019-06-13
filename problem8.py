1bc4554fa9539f616beefb7553b361dd7482f157
nampy3 <16tec2cs017@vgu.ac.in>
2019-06-13 19:58:11 +0530

    take the input from user and command must be stored in a FILE and success of command shown to monitor

---
+++ problem8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+from shutil import which
+#all the commands given by user either wrong or right must be store in a file
+location=input('Enter the location of file in which data must be stored: ')
+file=open(location,"a")
+command=input('Enter the command: ')
+file.write(command)
+# output of success command will be displayed
+if(which(command)):
+    print('success')
+    os.system(command)
+file.close()
+fr=open(location,'r')
+print(fr.read())
+fr.close()
